10d55088fffa2716084d255bacf07b713bc3a818
Neeraj Mathur <neeraj.mathur@gmail.com>
2016-02-13 17:51:37 +0530

    Added method to get diff snapshots

--- DetectLogicalDependency/Common/GitHelper.cs
+++ DetectLogicalDependency/Common/GitHelper.cs
@@ -19,11 +19,38 @@ namespace DetectLogicalDependency.Common
         public const string BaseFileForDependency ="CatalogController.cs";// "defaultResources.nopres.xml";
         public static string GetCommitDiff(Repository repository, Git git, RevCommit rev, String file)
         {
+
+            string snapshotN = GetSnapshot(repository, git, rev.GetParent(0).Tree, file);
+            string snapshotNPlusOne = GetSnapshot(repository, git, rev.Tree, file);
+
             //Fetch diffrence of commit
             OutputStream @out = new ByteArrayOutputStream();
             DiffCommand diff = git.Diff().SetPathFilter(PathFilter.Create(file)).SetOldTree(GetTreeIterator(rev.GetParent(0).Tree.Name, repository)).SetNewTree(GetTreeIterator(rev.Tree.Name, repository)).SetOutputStream(@out);
             IList<DiffEntry> entries = diff.Call();
             return @out.ToString();
+        }
+
+        private static string GetSnapshot(Repository repository, Git git, RevTree revTree, String file)
+        {
+            //Get snapshot , Retrive older version of an object
+            TreeWalk treeWalk = TreeWalk.ForPath(git.GetRepository(), file, revTree);
+            byte[] data = repository.Open(treeWalk.GetObjectId(0)).GetBytes();
+            return System.Text.Encoding.UTF8.GetString(data);
+
+
+            //ObjectLoader loader = repository.Open(rev.ToObjectId());
+            //InputStream ttt = loader.OpenStream();
+            //string result = ttt.ToString();
+            //Retrive older version of an object
+            //RevTree revTree = rev.Tree;
+            //TreeWalk treeWalk = new TreeWalk(git.GetRepository());
+            //treeWalk.AddTree(revTree);
+            //while (treeWalk.Next())
+            //{
+            //    //compare treeWalk.NameString yourself
+            //    byte[] bytes = treeWalk.ObjectReader.Open(rev.ToObjectId()).GetCachedBytes();
+            //    string result = System.Text.Encoding.UTF8.GetString(rev.RawBuffer);
+            //}
         }
 
         private static AbstractTreeIterator GetTreeIterator(string name, Repository db)
